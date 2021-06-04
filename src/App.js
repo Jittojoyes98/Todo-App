21bcd4f6b7f1f70d7dccd1b3546923bac466a890
ROLAND KIRALY <43180177+toobig4u@users.noreply.github.com>
2021-06-04 20:36:30 +0200

    ADD: custom checkbox
    HINT: you need working state in React to correctly style and use checkboxes!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import React,{useState} from "react"
-import Todoform from "./TodoForm"
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
 import Todo from './Todo';
 // import State from "./pages/State"
 // import Footer from "./pages/Footer"
@@ -13,41 +13,38 @@ import Todo from './Todo';
 
 // import Authourise from "./pages/Authorise"
 
-
 function App() {
-  const [ todoList, SetTodoList ] = useState([
+  const [todoList, SetTodoList] = useState([
     {
       id: 1,
       isCompleted: false,
-      text: "homework"
+      text: 'homework',
     },
     {
       id: 2,
       isCompleted: false,
-      text: "Assignment"
-    }
-  ]); 
+      text: 'Assignment',
+    },
+  ]);
   const addTodo = (todo) => {
-    SetTodoList([...todoList, { text: todo}]);
+    SetTodoList([...todoList, { text: todo }]);
     // console.log(todoList[todoList.length-1].id);
   };
-  const remove=(index)=>{
-    const newTodo=[...todoList];
-    newTodo[index].isCompleted=(!newTodo[index].isCompleted);
-    SetTodoList(newTodo)
-  }
+  const remove = (index) => {
+    const newTodo = [...todoList];
+    newTodo[index].isCompleted = !newTodo[index].isCompleted;
+    SetTodoList(newTodo);
+  };
   return (
-    <div className="App">
-      <div className="main-wrapper">
-        <Todoform addTodo={addTodo}/>
+    <div className='main-wrapper'>
+      <div className='card frame'>
+        <TodoForm addTodo={addTodo} />
         <Todo todoList={todoList} remove={remove} />
       </div>
     </div>
-    
   );
 }
 
 //  now this is great
-
 
 export default App;
